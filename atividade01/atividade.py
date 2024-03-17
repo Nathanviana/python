9d276cf6d91d82a365a1be74b2d7fadb239a0504
Nathan viana <viananathan03@gmail.com>
2024-03-17 19:20:25 -0300

    Atividade 01

---
+++ atividade01/atividade.py
@@ -0,0 +1,6 @@
+# Elabore um programa que imprima os seus dados pessoais, conforme exemplo abaixo:
+# Nome: <mostre seu nome aqui> Endereço: <mostre seu endereço aqui> Telefone: <mostre seu telefone aqui> 
+
+print("Nome: Nathan Viana")
+print("Endereço: Estados Unidos")
+print("Telefone: 985979679")
