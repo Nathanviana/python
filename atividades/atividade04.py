c872b27d3dac766800d1696550de90d39b32dc3c
Nathan viana <viananathan03@gmail.com>
2024-03-18 20:31:48 -0300

    atividade 04

---
+++ atividades/atividade04.py
@@ -0,0 +1,11 @@
+# Faça um programa que leia dois valores para as variáveis A e B e efetue a troca dos valores de forma que a variável A passe a possuir o valor da variável B e a variável B passe a possuir o valor da variável A Apresente os valores trocados. 
+
+a = 2
+b = 4
+
+c = a # c = 2
+
+a = b # a = 4
+b = c # b = 2
+
+print("Agora os valores de A e B foram trocados A agora é: {} e B é: {}".format(a,b))
