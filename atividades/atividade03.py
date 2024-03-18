01841e255b338eb51e800734de7b6ca276c593b3
Nathan viana <viananathan03@gmail.com>
2024-03-18 20:21:10 -0300

    atividade 03

---
+++ atividades/atividade03.py
@@ -0,0 +1,6 @@
+# Escreva um programa que receba do teclado um nome e sobrenome de um usuário e imprima a mensagem "Seja bem-vindo {nome} {sobrenome}". 
+
+nome = input('digite seu primeiro nome')
+sobrenome = input('digite seu sobrenome')
+
+print('Seja bem-vindo', nome, sobrenome)
