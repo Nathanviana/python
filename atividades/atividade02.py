ce9b97806deba80f33c2a9951a93daab250a57b0
Nathan viana <viananathan03@gmail.com>
2024-03-17 21:03:41 -0300

    atividade 02

---
+++ atividades/atividade02.py
@@ -0,0 +1,10 @@
+# Faça um programa que o usuário digite um número e obtenha como resultado o dobro e o triplo do número digitado. 
+
+num = int(input("Digite qualquer número e obtenha o dobro e o triplo dele"))
+
+
+dobro = num * 2
+triplo = num * 3
+
+print("O dobro do número é:", dobro)
+print("O triplo do número é:", triplo)
