84fa0fd89d7c0cc473aafeb22dc40f008762c7ac
Nathan viana <viananathan03@gmail.com>
2024-03-20 11:09:26 -0300

    atividade 08

---
+++ atividades/atividade08.py
@@ -0,0 +1,8 @@
+# Escreva um programa para converter uma temperatura de Celsius para Fahrenheit (F = C*1,8 + 32).
+
+
+celsius = float(input("Digite o valor em celsius que deve ser convertida"))
+
+fahrenheit = celsius * 1.8 + 32
+
+print(celsius, "convertido para fahrenheit é:", fahrenheit)
