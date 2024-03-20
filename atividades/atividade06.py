2c5116ab20f17e2985cd6c97a7edb60dc5ed127a
Nathan viana <viananathan03@gmail.com>
2024-03-20 10:56:49 -0300

    atividade06

---
+++ atividades/atividade06.py
@@ -0,0 +1,8 @@
+# Escreva um programa que o usuário digite o valor da base e da altura de um triângulo para calcular a sua área. 
+
+base = float(input("Digite uma base para o triângulo"))
+altura = float(input("Digite uma altura para o triângulo"))
+
+areaT = base * altura / 2
+
+print("A área do triângulo é", areaT)
