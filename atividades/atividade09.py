0563a1d2a5a4ef253a28991a10425864b8a0ca73
Nathan viana <viananathan03@gmail.com>
2024-03-20 11:19:39 -0300

    atividade 09

---
+++ atividades/atividade09.py
@@ -0,0 +1,8 @@
+# Escreva um programa para converter de libras para quilogramas. (k = l / 22046)
+
+
+valorEmLibras = 1000000
+
+k = valorEmLibras / 22046
+
+print(valorEmLibras, "Libras convertido em quilogramas é:", k)
