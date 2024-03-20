4dd820af1ebe6636053f6f96797269854332eb38
Nathan viana <viananathan03@gmail.com>
2024-03-20 12:51:58 -0300

    atividade 12

---
+++ atividades/atividade12.py
@@ -0,0 +1,8 @@
+# Elabore um programa para calcular e mostrar o valor da conversão de uma quantia em dólares para reais. Crie variáveis para guardar o valor da cotação do dólar do dia, o valor em dólares e o valor do resultado da conversão. Use a fórmula:quantiaEmReais = quantiaEmDolares * cotacaoDoDolar 
+
+cotacaoDoDolar = 5.01
+quantiaEmDolares = 15
+
+quantiaEmReais = quantiaEmDolares * cotacaoDoDolar
+
+print('{} dólares convertido para real é: {:.2f}' .format(quantiaEmDolares, quantiaEmReais))
