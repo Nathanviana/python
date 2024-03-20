68262350ce86e43f96fea5d14f33232d2460e307
Nathan viana <viananathan03@gmail.com>
2024-03-20 11:19:30 -0300

    atividade 10

---
+++ atividades/atividade10.py
@@ -0,0 +1,10 @@
+# Escreva um programa para calcular a média de quatro números. 
+
+num1 = 7
+num2 = 7
+num3 = 7
+num4 = 7
+
+media = (num1 + num2 + num3 + num4) / 4 
+
+print("A média dos 4 números é:", media)
