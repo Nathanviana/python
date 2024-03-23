8297376e614e2be770b1d121376e0a02ed0ec008
Nathan viana <viananathan03@gmail.com>
2024-03-22 23:36:51 -0300

    atividade 16

---
+++ atividades/atividade16.py
@@ -0,0 +1,10 @@
+# Escreva um algoritmo que receba 4 números e apresente o somatório deles.
+
+num1 = int(input("Digite um primeiro número"))
+num2 = int(input("Digite um segundo número"))
+num3 = int(input("Digite um terceiro número"))
+num4 = int(input("Digite um quarto número"))
+
+soma = num1 + num2 + num3 + num4
+
+print(soma)
