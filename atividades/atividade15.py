f1654c24a29f35413d1aaba0b409e60877a21893
Nathan viana <viananathan03@gmail.com>
2024-03-22 23:36:31 -0300

    atividade 15

---
+++ atividades/atividade15.py
@@ -0,0 +1,8 @@
+# Escreva um algoritmo que receba o salário de um funcionário, calcule e imprima o valor do imposto de renda a ser pago, sabendo que o imposto equivale a 5% do salário. 
+
+salario = float(input("Digite o valor do seu salário"))
+
+imposto = 5/100 * salario
+
+print('Valor do imposto a ser pago:', imposto)
+
