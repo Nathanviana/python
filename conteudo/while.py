431e4c8618f2ea6c4a9cb231019d0fd5f1138c83
Nathanviana <viananathan03@gmail.com>
2024-06-02 21:01:58 -0300

    estudos de while

---
+++ conteudo/while.py
@@ -0,0 +1,29 @@
+# while repetindo de 1 a 3
+# x=1
+# while x <= 3:
+#     print(x)
+#     x = x + 1
+    
+# while repetindo de 1 a 100
+# y=1
+# while y <= 100:
+#     print(y)
+#     y = y + 1
+    
+# while repetindo de 50 a 100
+# z=50 
+# while z <= 100:
+#     print(z)
+#     z = z + 1
+    
+# while contando descrecente 
+# q = 10
+# while q >= 1:
+#     print(q)
+#     q = q - 1
+
+# while contando numeros pares
+w = 0
+while w <= 10:
+    print(w)
+    w=w+2
