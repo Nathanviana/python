d783e97d0fa677a1183e107e1685633ce2b1f146
Nathanviana <viananathan03@gmail.com>
2024-06-05 13:49:03 -0300

    estudos de while e lista

---
+++ conteudo/listas.py
@@ -0,0 +1,15 @@
+# notas = [0,0,0,0]
+# contador = 0
+# soma = 0
+# while contador < 4:
+#     notas[contador]=float(input("digite sua nota: "))
+#     soma = soma + notas[contador]
+#     contador = contador + 1
+# media = soma/4
+# print(f"a media é: {media}")
+
+# lista com laço for
+l = [1,2,3,4]
+for i in range(0,len(l)):
+    print(i)
+    
